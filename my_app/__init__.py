db94f5b5ee20e18956c2a69282a0ad2dcade9c48
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2020-01-19 22:37:33 +0100

    first commit

---
+++ my_app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/LdapChat'
+app.config['WTF_CSRF_SECRET_KEY'] = 'random key for form'
+app.config['LDAP_PROVIDER_URL'] = 'ldap://192.168.28.135:389/'
+app.config['LDAP_PROTOCOL_VERSION'] = 3
+db = SQLAlchemy(app)
+
+app.secret_key = 'some_random_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from my_app.auth.views import auth
+
+app.register_blueprint(auth)
+
+db.create_all()
---
+++ my_app/auth/models.py
@@ -0,0 +1,44 @@
+import ldap
+from flask_wtf import Form
+from wtforms import TextField, PasswordField
+from wtforms.validators import InputRequired
+from my_app import db, app
+from flask_login._compat import unicode
+
+
+def get_ldap_connection():
+    conn = ldap.initialize(app.config['LDAP_PROVIDER_URL'])
+    return conn
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+
+    def __init__(self, username, password):
+        self.username = username
+
+    @staticmethod
+    def try_login(username, password):
+        conn = get_ldap_connection()
+        conn.simple_bind_s(
+            'cn=%s,cn=guest,ou=users,dc=chatroom,dc=com' % username,
+            password
+        )
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+
+class LoginForm(Form):
+    username = TextField('Username', [InputRequired()])
+    password = PasswordField('Password', [InputRequired()])
